7f077029fbe6fe3cc26b6ab53459fc7f02b41a64
root <root@DESKTOP-6BE3G1B.localdomain>
2022-06-30 19:03:32 -0300

    🎨 :art: [Refactor] Return only useful Pokemon props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
-import { getOptionsForVote } from '@/utils/getRandomPokemon'
+import { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
+import { trpc } from '@/utils/trpc'
 
 const Home: NextPage = () => {
-  const [first, second] = getOptionsForVote()
- 
+  const [pokemonIDs, setPokemonIDs] = useState<number[]>([])
+
+  useEffect(() => {
+    const [first, second] = getOptionsForVote()
+
+    setPokemonIDs([first, second])
+  }, [])
+
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: pokemonIDs[0] }])
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: pokemonIDs[1] }])
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">
@@ -11,12 +22,18 @@ const Home: NextPage = () => {
       </div>
       <div className="p-2"></div>
       <div className="border rounded p-8 justify-between max-w-2xl flex items-center">
-        <div className="w-16 h-16 bg-red-800">
-          {first}
+        <div className="w-64 h-64 flex flex-col">
+          <img
+          className="w-full"
+           src={firstPokemon.data?.sprites.front_default as string} alt="" />
+           <p className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</p>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-800">
-          {second}
+        <div className="w-64 h-64 flex flex-col">
+          <img
+          className="w-full"
+           src={secondPokemon.data?.sprites.front_default as string} alt="" />
+           <p className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</p>
         </div>
       </div>
     </div>
