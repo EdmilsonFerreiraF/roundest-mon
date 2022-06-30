cc5e8eea28cc12fd5a212dbb358e0dfb013b6e04
root <root@DESKTOP-6BE3G1B.localdomain>
2022-06-30 17:17:27 -0300

    🚀 :rocket: [Add] getRandomPokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,23 @@
-import { trpc } from '@/utils/trpc'
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const {data, isLoading} = trpc.useQuery(["hello", {text: "Edmilson"}])
-
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (data) {
-    return <div>{data.greeting}</div>
-  }
-  
+  const [first, second] = getOptionsForVote()
+ 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      Hello world
       <div className="text-2xl text-center">
         Which Pokémon is Rounded?
       </div>
       <div className="p-2"></div>
       <div className="border rounded p-8 justify-between max-w-2xl flex items-center">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">
+          {first}
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">
+          {second}
+        </div>
       </div>
     </div>
   )
